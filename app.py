d4d7baea6be0f96d12e74702019452b5ca5af920
lucas.desfontaine <lucas.desfontaine1@gmail.com>
2023-06-10 00:27:57 +1100

    fix(API): add function

--- app.py
+++ app.py
@@ -9,49 +9,48 @@ app = Flask(__name__)
 @app.route('/save', methods=['POST'])
 def save_data():
     try:
-        data = request.get_json()
-        timestamp = datetime.now().strftime("%Y%m%d%H%M%S%f")
-        username = data.get("username")  # Récupérer le nom d'utilisateur depuis les données reçues
-        data_with_timestamp = {
-            "timestamp": timestamp,
-            "username": username,
-            "data": data
-        }
-        if os.path.exists("save.json"):
-            with open("save.json", "a") as f:
-                f.write(json.dumps(data_with_timestamp,indent=4))
-        else:
-            with open("save.json", "w") as f:
-                f.write(json.dumps(data_with_timestamp,indent=4))
-        return "OK", 200
+        get_data()
+        checkFile()
+        # write_data()
+        return data_with_timestamp, 200
     except:
-        return "404", 404
+        return "Error 400: Bad Request"
 
 
-# ---------------------------------------------------ListJSON---------------------------------------------------
-@app.route('/list', methods=['GET'])
-def list_data():
-    # Si le fichier save.json existe, on le lit et on renvoie son contenu
-    if os.path.exists("save.json"):
-        with open("save.json", "r") as f:
-            return json.dumps(f.read()), 200
-    else:
-        return "404", 404
+
+def get_data():
+    global data_with_timestamp
+    try:
+        data = request.get_json()
+        data_without_name = data["data"]
+        data_with_timestamp = {
+            "timestamp": datetime.now().strftime("%d/%m/%Y %H:%M:%S"),
+            "username": data["username"],
+            "data": data_without_name}
+        return data_with_timestamp, 200
+    except:
+        return "Error 400: Bad Request"
 
 
-# ---------------------------------------------------GetJSONByUsername---------------------------------------------------
-@app.route('/get/<username>', methods=['GET'])
-def get_data(username):
-    # Si le fichier save.json existe, on le lit et on renvoie les données correspondant au nom d'utilisateur
-    if os.path.exists("save.json"):
-        with open("save.json", "r") as f:
-            for line in f:
-                data_with_timestamp = json.loads(line)
-                if data_with_timestamp["username"] == username:
-                    return json.dumps(data_with_timestamp["data"]), 200
-            return "404", 404
-    else:
-        return "404", 404
+def checkFile():
+    if not os.path.exists("save.json"):
+        with open("save.json", "w") as file:
+            file.write("")
+    return "File checked"
+
+# def write_data():
+    #         users = [user["username"] for user in data]
+    #         if data_with_timestamp["username"] in users:
+    #             index = users.index(data_with_timestamp["username"])
+    #             data[index]["data"].append(data_with_timestamp["data"])
+    #         else:
+    #             data.append(data_with_timestamp)
+    #     with open("save.json", "w") as file:
+    #         json.dump(data, file)
+    #     return "Data written", 200
+    # except:
+    #     return "Error 400: Bad Request"
+
 
 
 if __name__ == '__main__':
